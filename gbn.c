98b1b77b70fc47bb74da108557269c3d11617a34
AgniswarPaul <agniswar6699@gmail.com>
2022-12-02 14:39:21 -0500

    updated

--- gbn.c
+++ gbn.c
@@ -1,159 +1,168 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
 
-float TIMEOUT = 20.0;
+using namespace std;
+/* ******************************************************************
+ ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
 
-int next_position;
-int next_seq_num=0;
-int base_val;
-int window_size;
-int request_seq_num;
-int buffer=0;
-int sequence_number = 0;
-int last_packet_sent = 0;
+   This code should be used for PA2, unidirectional data transfer 
+   protocols (from A to B). Network properties:
+   - one way network delay averages five time units (longer if there
+     are other messages in the channel for GBN), but can be larger
+   - packets can be corrupted (either the header or the data portion)
+     or lost, according to user-defined probabilities
+   - packets will be delivered in the order in which they were sent
+     (although some can be lost).
+**********************************************************************/
 
-
-struct msg message_last;
-struct msg acknowledgement_ack;
- 
-
-struct packet_details{
-    struct pkt packet;
-    bool packet_sent;
-    bool acknowledgement;
-}
-
-struct packet_details *packetdata;
-
-
-void A_output(struct msg message){
-    struct pkt packet;
-    packet.seqnum = A.seq_A;
-    packet.acknum = 0;
-    strncpy(packet.payload,message.data, 20);
-    packet.checksum = get_checksum(packet);
-    packetdata[next_position].packet = packet;
-    packetdata[next_position].packet_sent = false;
-    packetdata[next_position].acknowledgement = false;
-    if(next_seq_num < base_val + window_size){
-        if(next_seq_num == base_val){
-            starttimer(0, TIMEOUT);
-        }
-        if(packetdata[next_seq_num].packet_sent == false && packetdata[next_seq_num].acknowledgement == false){
-            tolayer3(0, packetdata[next_seq_num].packet);
-            
-        }
-    }
-
-    next_seq_num++;
-    next_position++;
-    A.seq_A++;
-
-}
-
-
-
-
-
-
+/********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
 struct HostA
 {
     int seq_A;
     int ack_A;
-    int next_seq_A;
-    int base_number;
-    int next_buffer; 
-    int size_of_window;
-    struct pkt buffer_pakcet_A;
 } A;
+
 
 struct HostB
 {
     int seq_B;
-    int ack_B;
-    struct pkt B;
 } B;
 
-struct msg
-{
-    char data[20];
-};
+struct pkt livepacket;
 
-struct pkt
-{
-    int seqnum;
-    int acknum;
-    int checksum;
-    char payload[20];
-};
+int size_of_window, base_of_window; 
 
-int get_checksum(struct pkt packet)
+int next_seq_num;
+  
+static const float RTT = 40.0; 
+
+vector <msg> buffer;
+
+int make_the_checksum(struct pkt packet)
 {
     int checksum = 0;
     checksum += packet.seqnum;
     checksum += packet.acknum;
-    for (int i = 0; i < 20; ++i)
+    for (int i = 0; i < 20; i++){
         checksum += packet.payload[i];
+    }
     return checksum;
 }
 
-void A_init() 
-{
-    size_of_window=N;
-
-}
-
-void A_output(struct msg message)
-{
-    if (A.A_is_ready_to_transmit)
-    {
-        struct pkt packet;
-        packet.sequence_number = A.seq_A;
-        packet.ackowledgement_number = A.ack_A;
-        strncpy(packet.payload, message.data, 20);
-        packet.checksum = get_checksum(packet);
-        A.buffer_packet_A = packet;
-        tolayer3(0, packet);
-        starttimer(0, TIMEOUT);
-    }
-    else
-    {
-        printf("Host A is not ready to receive any new message");
-    }
-}
-
-void A_timerinterrupt()
-{
-    A.A_is_ready_to_transmit = false;
-    tolayer3(0, A.buffer_packet_A);
-    starttimer(0, TIMEOUT);
-}
-
-void A_input(struct pkt packet) {
-    if (ispacket_not_corrupt(packet)){
-        if(packet.acknum == A.seq_A){
-            stoptimer(0);
-            A.seq_A = 1 - A.seq_A;
-            A.A_is_ready_to_transmit = false;
-        }
+bool is_packet_corrupt(struct pkt packet){
+    if(packet.checksum == make_the_checksum(packet)){
+        return false;
     }
     else{
-        printf("packet is corrupted\n");
+        return true;
     }
- 
+        
 }
 
-void B_init(){
-    B.seq_B = 0;
+
+void packetmessage(msg message, int seqnum, int acknum)
+{
+
+    pkt livepacket = {};
+    strncpy(livepacket.payload, message.data, sizeof(livepacket.payload));
+    livepacket.seqnum = seqnum;
+    livepacket.acknum = acknum;
+    livepacket.checksum = make_the_checksum(livepacket);
+    tolayer3(0,livepacket);
+
 }
 
-void B_input(struct pkt packet) {
-    tolayer5(1,packet.payload);
-    B.B.acknum=B.seq_B;
-    B.B.checksum=get_checksum(packet);
-    tolayer3(1,B.B);
-    B.seq_B++;  
+
+void makeack(int acknum)
+{
+    pkt ackpacket = {};
+    ackpacket.acknum = acknum;
+    ackpacket.checksum = make_the_checksum(ackpacket);
+    tolayer3(1,ackpacket);
 }
 
+
+void usegbn()
+{
+    while((next_seq_num < buffer.size()) && (next_seq_num < base_of_window + size_of_window))
+    {
+        packetmessage(buffer[next_seq_num],next_seq_num,A.ack_A);
+        
+
+        if(base_of_window == next_seq_num)
+            starttimer(0,RTT);
+    next_seq_num++;    
+    }
+    
+}
+
+
+/* called from layer 5, passed the data to be sent to other side */
+void A_output(struct msg message)
+{
+	buffer.push_back(message);
+    	usegbn();
+}
+
+/* called from layer 3, when a packet arrives for layer 4 */
+void A_input(struct pkt packet)
+{
+    if(!is_packet_corrupt(packet))
+    {
+        base_of_window = packet.acknum + 1;
+
+        if(base_of_window == next_seq_num)
+            stoptimer(0);
+        else
+        {
+            stoptimer(0);
+            starttimer(0, RTT);
+        }
+    }
+}
+
+/* called when A's timer goes off */
+void A_timerinterrupt()
+{
+	next_seq_num = base_of_window;
+    	usegbn();
+}  
+
+/* the following routine will be called once (only) before any other */
+/* entity A routines are called. You can use it to do any initialization */
+void A_init()
+{
+    A.seq_A = 0;
+    A.ack_A = 0;
+    size_of_window = getwinsize();
+    base_of_window = 0;
+    next_seq_num = 0;
+}
+
+/* Note that with simplex transfer from a-to-B, there is no B_output() */
+
+/* called from layer 3, when a packet arrives for layer 4 at B*/
+
+
+
+
+void B_input(struct pkt packet)
+{
+	if(!is_packet_corrupt(packet) && (B.seq_B == packet.seqnum))
+    	{
+		tolayer5(1,packet.payload);
+		makeack(B.seq_B);
+        B.seq_B++;
+    	}
+}
+    
+
+/* the following rouytine will be called once (only) before any other */
+/* entity B routines are called. You can use it to do any initialization */
+void B_init()
+{
+     B.seq_B = 0;
+}
